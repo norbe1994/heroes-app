82f69b6e723d2a0af55772f7161a39deff8314b1
Norberto Caceres <norbe1994@yahoo.com>
2019-04-08 09:34:33 -0500

    crear componentes heores y heroes y rutaje basico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { APP_ROUTING } from './app.routes'
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroes/heroe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    APP_ROUTING,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { RouterModule, Routes } from '@angular/router'
+import { HeroesComponent } from '../app/components/heroes/heroes.component';
+import { HeroeComponent } from '../app/components/heroes/heroe.component';
+
+const APP_ROUTES: Routes = [
+    { path: 'heroes', component: HeroesComponent },
+    { path: 'heroes/:id', component: HeroeComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'heroes' },
+]
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES)
